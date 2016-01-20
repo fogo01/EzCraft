f30f81cc08dadc22147dcec9a533e7eca86f0543
Axel Hedvall <axel.hedvall@gmail.com>
2016-01-20 14:21:20 +0100

    Homing Missile
    Som work on the homing missile and the launcer

--- src/main/java/com/fogo01/ezcraft/entities/EntityHomingMissile.java
+++ src/main/java/com/fogo01/ezcraft/entities/EntityHomingMissile.java
@@ -6,10 +6,12 @@ import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
+
 import java.util.List;
 
 public class EntityHomingMissile extends EntityThrowable {
     private int life = 0;
+    public EntityLivingBase target = null;
 
     public EntityHomingMissile(World par1World) {
         super(par1World);
@@ -17,6 +19,11 @@ public class EntityHomingMissile extends EntityThrowable {
 
     public EntityHomingMissile(World par1World, EntityLivingBase par2EntityLivingBase) {
         super(par1World, par2EntityLivingBase);
+    }
+
+    public EntityHomingMissile(World par1World, EntityLivingBase par2EntityLivingBase, EntityLivingBase target) {
+        super(par1World, par2EntityLivingBase);
+        this.target = target;
     }
 
     public EntityHomingMissile(World par1World, double par2, double par4, double par6) {
@@ -48,37 +55,38 @@ public class EntityHomingMissile extends EntityThrowable {
         this.worldObj.spawnParticle("flame", this.posX, this.posY, this.posZ, -this.motionX / 10, -this.motionY / 10, -this.motionZ / 10);
         this.worldObj.spawnParticle("smoke", this.posX + Math.random() - 0.5, this.posY + Math.random() - 0.5, this.posZ + Math.random() - 0.5, 0, 0, 0);
 
-        //-------
-        AxisAlignedBB AABB = AxisAlignedBB.getBoundingBox(this.posX - 10, this.posY - 10, this.posZ - 10, this.posX + 10, this.posY + 10, this.posZ + 10);
+        EntityLivingBase localTarget = null;
+        if (target == null) {
+            AxisAlignedBB AABB = AxisAlignedBB.getBoundingBox(this.posX - 10, this.posY - 10, this.posZ - 10, this.posX + 10, this.posY + 10, this.posZ + 10);
 
-        List entities = this.worldObj.getEntitiesWithinAABB(EntityLivingBase.class, AABB);
-        EntityLivingBase entity;
-        EntityLivingBase entity1 = null;
-        double distance = 0;
-        for (int i = 0; i < entities.size(); ++i) {
-            entity = (EntityLivingBase)entities.get(i);
+            List entities = this.worldObj.getEntitiesWithinAABB(EntityLivingBase.class, AABB);
+            EntityLivingBase entity;
+            double distance = 0;
+            for (int i = 0; i < entities.size(); ++i) {
+                entity = (EntityLivingBase) entities.get(i);
 
-            double X = this.posX - entity.posX;
-            double Y = this.posY - entity.posY;
-            double Z = this.posZ - entity.posZ;
+                double X = this.posX - entity.posX;
+                double Y = this.posY - entity.posY + 1;
+                double Z = this.posZ - entity.posZ;
 
-            if (getDistanceSq(X, Y, Z) > distance && entity != this.getThrower()) {
-                entity1 = (EntityLivingBase) entities.get(i);
-                distance = getDistanceSq(X, Y, Z);
+                if (getDistanceSq(X, Y, Z) > distance && entity != this.getThrower()) {
+                    localTarget = (EntityLivingBase) entities.get(i);
+                    distance = getDistanceSq(X, Y, Z);
+                }
             }
+
+        } else {
+            localTarget = target;
         }
+            LogHelper.info(localTarget);
 
-        LogHelper.info(entity1);
-
-        if (entity1 != null) {
-            double X = entity1.posX - this.posX;
-            double Y = entity1.posY - this.posY;
-            double Z = entity1.posZ - this.posZ;
-            float f = 0.1f;
-            this.addVelocity(X * f, Y * f, Z * f);
-        }
-        //----------
-
+            if (localTarget != null) {
+                double X = localTarget.posX - this.posX;
+                double Y = localTarget.posY - this.posY;
+                double Z = localTarget.posZ - this.posZ;
+                float f = 0.1f;
+                this.addVelocity(X * f, Y * f, Z * f);
+            }
 
         if(life >= 100) {
             this.setDead();
