693c058efe3f4f28eb25da10bd1f59025fe23561
fogo01 <axel.hedvall@gmail.com>
2016-01-25 21:21:20 +0100

    Textures and names

--- src/main/java/com/fogo01/ezcraft/block/BlockInfiniWater.java
+++ src/main/java/com/fogo01/ezcraft/block/BlockInfiniWater.java
@@ -1,11 +1,21 @@
 package com.fogo01.ezcraft.block;
 
+import com.fogo01.ezcraft.reference.Reference;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 public class BlockInfiniWater extends BlockEzCraft {
+    @SideOnly(Side.CLIENT)
+    private IIcon iconTop;
+    @SideOnly(Side.CLIENT)
+    private IIcon iconBottom;
+
     public BlockInfiniWater() {
         super();
         this.setBlockName("InfiniWater");
@@ -23,5 +33,17 @@ public class BlockInfiniWater extends BlockEzCraft {
             }
         }
         return true;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side, int metadata) {
+        return side == 1 ? this.iconTop : (side == 0 ? this.iconBottom : this.blockIcon);
+    }
+
+    @Override
+    public void registerBlockIcons(IIconRegister iconRegister) {
+        blockIcon = iconRegister.registerIcon(Reference.MOD_ID + ":InfiniWater");
+        iconTop = iconRegister.registerIcon(Reference.MOD_ID + ":InfiniWaterTop");
+        iconBottom = iconRegister.registerIcon(Reference.MOD_ID + ":InfiniWaterBottom");
     }
 }
