4c64653c5440d28f7b6c4a7e1bafc716f928c6ac
fogo01 <axel.hedvall@gmail.com>
2016-01-30 12:25:15 +0100

    Wormhole
    Wormehole types

--- src/main/java/com/fogo01/ezcraft/tileEntity/TileEntityWormHole.java
+++ src/main/java/com/fogo01/ezcraft/tileEntity/TileEntityWormHole.java
@@ -26,6 +26,7 @@ public class TileEntityWormHole extends TileEntity {
         LogHelper.info("Write");
         super.writeToNBT(nbtTagCompound);
         nbtTagCompound.setDouble("radiusMulti", radiusMulti);
+        nbtTagCompound.setString("type", type);
     }
 
     @Override
@@ -33,6 +34,7 @@ public class TileEntityWormHole extends TileEntity {
         LogHelper.info("Read");
         super.readFromNBT(nbtTagCompound);
         radiusMulti = nbtTagCompound.getDouble("radiusMulti");
+        type = nbtTagCompound.getString("type");
     }
 
     @Override
@@ -43,7 +45,7 @@ public class TileEntityWormHole extends TileEntity {
             for (int y = yCoord - (int)radius; y < yCoord + radius; y++) {
                 for (int z = zCoord - (int)radius; z < zCoord + radius; z++) {
                     if (Math.random() < 0.01)
-                        worldObj.spawnParticle("smoke", x + 0.5f,y + 0.5f, z + 0.5f, 0, 0, 0);
+                        worldObj.spawnParticle("smoke", x + 0.5f, y + 0.5f, z + 0.5f, 0, 0, 0);
                 }
             }
         }
@@ -51,26 +53,47 @@ public class TileEntityWormHole extends TileEntity {
         AxisAlignedBB AABB = AxisAlignedBB.getBoundingBox(centreX - radius, centreY - radius, centreZ - radius, centreX + radius, centreY + radius, centreZ + radius);
         List list = worldObj.getEntitiesWithinAABB(Entity.class, AABB);
 
-        float f = 0;
         Entity entity;
         for (int i = 0; i < list.size(); ++i) {
+            float f = 0.0f;
             entity = (Entity)list.get(i);
             if (entity instanceof EntityPlayer) {
                 EntityPlayer player = (EntityPlayer) entity;
-                if (player.getCurrentArmor(0) != null && player.getCurrentArmor(0).getItem() != ModItems.LeadBoots)
-                    f = 0.04f;
+                boolean flag = false;
+                if (player.getCurrentArmor(0) != null) {
+                    if (player.getCurrentArmor(0).getItem() == ModItems.LeadBoots)
+                        flag = true;
+                }
+                if (!flag)
+                    f = 0.05f;
             } else {
                 f = 0.075f;
             }
             if (entity instanceof EntityLivingBase) {
+                boolean flag = false;
                 EntityLivingBase entityLiving = (EntityLivingBase)entity;
-                if (type == ReferenceWormHole.types[0]) {
-                    entityLiving.addPotionEffect(new PotionEffect(Potion.poison.id, 20 * 5, 0));
-                    entityLiving.addPotionEffect(new PotionEffect(Potion.hunger.id, 20 * 5, 0));
-                }  else if (type == ReferenceWormHole.types[1]) {
-                    entityLiving.addPotionEffect(new PotionEffect(Potion.confusion.id, 20 * 5, 1));
+                if (entity instanceof EntityPlayer) {
+                    EntityPlayer player = (EntityPlayer) entity;
+                    if (player.getCurrentArmor(0) != null) {
+                        if (player.getCurrentArmor(0).getItem() == ModItems.LeadBoots)
+                            flag = true;
+                    }
+                }
+
+                if (!flag) {
+                    if (type == ReferenceWormHole.types[4]) {
+                        entityLiving.addPotionEffect(new PotionEffect(Potion.poison.id, 20 * 5, 0));
+                        entityLiving.addPotionEffect(new PotionEffect(Potion.hunger.id, 20 * 5, 0));
+                    } else if (type == ReferenceWormHole.types[5]) {
+                        entityLiving.addPotionEffect(new PotionEffect(Potion.confusion.id, 20 * 5, 1));
+                    } else if (type == ReferenceWormHole.types[6]) {
+                        entityLiving.addPotionEffect(new PotionEffect(Potion.wither.id, 20 * 5, 0));
+                    }
                 }
             }
+            if (type == ReferenceWormHole.types[1])
+                f *= 1.8;
+
             //f -= entity.getDistanceSq(centreX, centreY, centreZ) * 0.0001;
 
             double X = centreX - entity.posX;
@@ -83,9 +106,12 @@ public class TileEntityWormHole extends TileEntity {
         AABB = AxisAlignedBB.getBoundingBox(centreX - dmgRadius, centreY - dmgRadius, centreZ - dmgRadius, centreX + dmgRadius, centreY + dmgRadius, centreZ + dmgRadius);
         list = worldObj.getEntitiesWithinAABB(EntityLivingBase.class, AABB);
 
+        float dmg = 1.0f;
+        if (type == ReferenceWormHole.types[2])
+            dmg *= 2;
         for (int i = 0; i < list.size(); ++i) {
             entity = (Entity)list.get(i);
-            entity.attackEntityFrom(Reference.DMG_WORMHOLE, 1.0f);
+            entity.attackEntityFrom(Reference.DMG_WORMHOLE, dmg);
         }
     }
 }
